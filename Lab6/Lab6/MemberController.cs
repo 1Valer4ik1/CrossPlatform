9512a5776f6ba44abbb2a7e6d9671b80922bf971
1Valer4ik1 <valera05744@gmail.com>
2024-11-21 21:45:30 +0200

    Add files via upload

---
+++ Lab6/Lab6/MemberController.cs
@@ -0,0 +1,29 @@
+﻿using GlobalCommunity.Models;
+using Microsoft.AspNetCore.Mvc;
+
+public class MemberController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public MemberController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var members = await _context.Members.Include(m => m.LocalCommunity).ToListAsync();
+        return View(members);
+    }
+
+    public async Task<IActionResult> Details(int id)
+    {
+        var member = await _context.Members
+            .Include(m => m.LocalCommunity)
+            .FirstOrDefaultAsync(m => m.Id == id);
+
+        if (member == null) return NotFound();
+
+        return View(member);
+    }
+}
