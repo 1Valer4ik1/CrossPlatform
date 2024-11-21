55de0e848458f584001524a859b3d8f17d05a49f
1Valer4ik1 <valera05744@gmail.com>
2024-11-21 20:57:16 +0200

    Add files via upload

---
+++ Laba5/WebAppController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(); // Створіть представлення Index.cshtml
+        }
+
+        public IActionResult About()
+        {
+            return View(); // Створіть представлення About.cshtml з описом роботи
+        }
+    }
+}
